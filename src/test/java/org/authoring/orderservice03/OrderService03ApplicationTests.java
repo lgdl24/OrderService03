bf4218c9b5a31ff9295632b1814c1cefe990d2c7
lgdl24 <rkfnal77@skuniv.ac.kr>
2026-08-07 09:40:03 +0900

    초기세팅

---
+++ src/test/java/org/authoring/orderservice03/OrderService03ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.authoring.orderservice03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderService03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
